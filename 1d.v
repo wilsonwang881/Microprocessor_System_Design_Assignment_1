c852cc737c6cf227f11d29ea1c147fb48a5f3aac
Wilson Wang <Wilson.Wang@wilsonwang.org>
2020-02-13 23:35:39 -0600

    Updates from Overleaf

--- 1d.v
+++ 1d.v
@@ -6,11 +6,11 @@ module gate_combo_1_d (e, f, a, b, c, d);
     // use an always block to detect changes and update
     always @ (a or b or c or d)
     begin
-        x <= ~a;
-        y <= a & b;
-        z <= y | c;
-        e <= x | z;
-        f <= ~(z & d);
+        x = ~a;
+        y = a & b;
+        z = y | c;
+        e = x | z;
+        f = ~(z & d);
     end
 
 endmodule
